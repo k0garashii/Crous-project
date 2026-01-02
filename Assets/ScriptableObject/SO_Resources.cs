cf4c749e2b8070affda8e43ac391c1c23c55e25c
Menez sasha <s.menez@student.isartdigital.com>
2026-01-02 15:04:01 +0100

    add: building infos

---
+++ Assets/ScriptableObject/SO_Resources.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Resource", menuName = "Scriptable Objects/Resources")]
+public class SO_Resources : ScriptableObject
+{
+    public int wood;
+    public int stone;
+    public int clay;
+    public int iron;
+    public int gold;
+    public int food;
+    public int water;
+    public int energy;
+    public int reasearchPoints;
+    public int happiness;
+    public int population;
+}
---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int wood;
+    public int stone;
+    public int clay;
+    public int iron;
+    public int gold;
+    public int food;
+    public int water;
+    public int energy;
+    public int reasearchPoints;
+    public int happiness;
+    public int population;
+
+    [HideInInspector] public static ResourceManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
