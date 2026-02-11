69789ff160789fb5372713e3eb0101010e64a2df
Alma <delplace.martin1@gmail.com>
2026-02-11 14:51:04 +0100

    Builder

--- Assets/Scripts/BuilderAgent.cs
+++ Assets/Scripts/BuilderAgent.cs
@@ -7,73 +7,30 @@ public class BuilderAgent : MonoBehaviour
     public State actualState;
     private State lastState;
 
-    public AIDestinationSetter destinationSetter { get; set; }
-    public AIPath aiPath { get; set; }
+    public LLM_Manager llmManager;
+
+    public string currentObjective;
+
+    public AIDestinationSetter destinationSetter { get; private set; }
+    public AIPath aiPath { get; private set; }
+
     public int targetX { get; set; }
     public int targetY { get; set; }
-
-    // ===== House =====
-    private House currentHouse;
-    private bool isInside = false;
 
     void Awake()
     {
         destinationSetter = GetComponent<AIDestinationSetter>();
         aiPath = GetComponent<AIPath>();
-        actualState.OnEnter(this);
     }
 
-    void Update() 
+    void Update()
     {
-        if (lastState != actualState)
+        if (actualState != lastState)
         {
-            actualState.OnEnter(this);
+            actualState?.OnEnter(this);
             lastState = actualState;
         }
-        if(actualState)
-            actualState.HandleUpdate();
-    }
 
-    public void ToggleHouse(House house)
-    {
-        if (!house) return;
-
-        if (!isInside)
-            EnterHouse(house);
-        else
-            ExitHouse();
-    }
-
-    void EnterHouse(House house)
-    {
-        if (!house.CanEnter()) return;
-
-        currentHouse = house;
-        house.AddOccupant(this);
-
-        gameObject.SetActive(false);
-        isInside = true;
-
-        Debug.Log(" Builder entré dans la maison");
-    }
-
-    void ExitHouse()
-    {
-        if (currentHouse == null) return;
-
-        Vector3 exitPos = currentHouse.GetSafeExitPosition();
-        transform.position = exitPos + Vector3.up * 0.1f;
-
-        currentHouse.RemoveOccupant(this);
-
-        gameObject.SetActive(true);
-        isInside = false;
-        currentHouse = null;
-
-        destinationSetter.target = null;
-        aiPath.canMove = true;
-        aiPath.SearchPath();
-
-        Debug.Log(" Builder sorti de la maison");
+        actualState?.HandleUpdate();
     }
 }
